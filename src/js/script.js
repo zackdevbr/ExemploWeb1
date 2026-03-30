c2e3053db5a34117fa89413c76104df07761d41e
Isac <contatoisacn@gmail.com>
2026-03-30 09:15:41 -0300

    feat: Declarações e Variáveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,44 @@
-alert("Hello world !!!")
+// Declarações e Variáveis
+// var, let e const
+
+var nome = "Fiap";
+console.log(nome);
+
+var nome = "Teste";
+console.log(nome);
+
+if(true){
+    let apelido = "Fiapinho";
+    console.log(apelido);
+}
+
+const taxaJuros = 0.05;
+console.log(taxaJuros);
+
+//typeof - verifica o tipo da variável
+var exemplo = "Olá dev-var";
+console.log(typeof exemplo);
+let exemplo1 = "Olá dev-let";
+console.log(exemplo1);
+const exemplo2 = "Olá dev-const";
+console.log(exemplo2);
+
+let exemplo3 = 10;
+console.log(typeof exemplo3);
+
+let exemplo4 = true;
+console.log(typeof exemplo4);
+
+let exemplo5 = {nome1: "teste"};
+console.log(typeof exemplo5);
+
+let exemplo6 = ["Java", "C#", "PHP"];
+console.log(exemplo6);
+
+// valor undefined
+let exemplo7;
+console.log(exemplo7);
+
+//valor vazio
+let exemplo8 = null;
+console.log(exemplo8);
